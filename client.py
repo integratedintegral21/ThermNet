e4183e32ef36c8c2053b43fe54f086afc3920667
IntegratedIntegral <w.boncela@gmail.com>
2019-11-24 19:31:41 +0100

    cos tam

--- client.py
+++ client.py
@@ -1,1 +1,20 @@
-print("Hello client")
+import socket
+import time
+import w1thermsensor
+
+HOST, PORT = '192.168.8.105', 12345
+msg = 'Hello from the other side!'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+therm = w1thermsensor.W1ThermSensor()
+
+try:
+    s.connect((HOST,PORT))
+    s.sendall(msg.encode())
+    while True:
+        msg = round(therm.get_temperature(),1)
+        s.send(str(msg))
+        time.sleep(1)
+
+finally:
+    s.close()
--- server.py
+++ server.py
@@ -1,1 +1,14 @@
-print("Hello server")
+import socket
+import socketserver
+
+HOST, PORT = '192.168.8.105', 12345
+
+class TCPHandler(socketserver.BaseRequestHandler):
+    
+    def handle(self):
+        while True:
+            self.data = self.request.recv(1024).strip().decode()
+            print("Received: ", self.data, "from: "+ format(self.client_address[0]) +"")
+                
+server = socketserver.TCPServer((HOST, PORT),TCPHandler)
+server.serve_forever()
