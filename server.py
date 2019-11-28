e67f8ceb67f2878ca76ff144b2cc08ed163f9e1f
IntegratedIntegral <w.boncela@gmail.com>
2019-11-28 16:57:36 +0100

    Now threading is not as silly as it used to be

--- server.py
+++ server.py
@@ -3,15 +3,27 @@ import threading
 import sys
 
 host, port = '192.168.8.105', 12345
-threads = list()
+clients = list()
 
-def handleClient(conn,addr):
-    while True:
-        data = conn.recv(1024)
-        print(f"Message from:{address}")
-        print(data.decode())
-    print(f"Connection with: {address} closed")
-    conn.close()
+class Client(threading.Thread):
+
+    conn = 0
+    addr = 0
+
+    def handleClient(self,conn,addr):
+        while True:
+            data = conn.recv(1024)
+            print(f"Message from:{address}")
+            print(data.decode())
+        print(f"Connection with: {address} closed")
+        conn.close()
+
+    def __init__(self,conn,addr):
+        self.conn = conn
+        self.addr = addr
+        super().__init__(target=self.handleClient,args=(self.conn,self.addr,))
+        self.start()
+    
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -27,5 +39,5 @@ s.listen(10)
 while True:
     connection, address = s.accept()
     print(f"Connected with {address}")
-    threads.append(threading.Thread(target=handleClient,args=(connection,address,)))
-    threads[len(threads) - 1].start()
+    new_client = Client(connection,address)
+    clients.append(new_client)
